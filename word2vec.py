e6196a7d0e1cafe32fee6f04183baf205f66de00
jovan-ioanis <jovan.nikolic@gess.ethz.ch>
2017-05-28 11:36:51 +0200

    initial commit

---
+++ word2vec.py
@@ -0,0 +1,93 @@
+import gensim
+import logging
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+TRAIN_FILE = "data/Training_Shuffled_Dataset.txt"
+VALIDATION_FILE = "data/Validation_Shuffled_Dataset.txt"
+WORD2VEC = "word2vec/wordembeddings_200.word2vec"
+
+EMBEDDING_SIZE = 200
+MINIMAL_WORD_FREQUENCY = 1
+
+
+
+def load_sentences(train_path, validation_path=None):
+    sentences = []
+
+    f = open(train_path, 'r')
+    for line in f:
+        conversation = line.strip().split('\t')
+        for i in range(3):
+            sentence = []
+            for word in conversation[i].split():
+                sentence.append(word)
+            sentences.append(sentence)
+
+    if validation_path is not None:
+        f = open(VALIDATION_FILE, 'r')
+        for line in f:
+            conversation = line.strip().split('\t')
+            for i in range(3):
+                sentence = []
+                for word in conversation[i].split():
+                    sentence.append(word)
+                sentences.append(sentence)
+
+    return sentences
+
+
+def train_embeddings(save_to_path):
+    model = gensim.models.Word2Vec(load_sentences(TRAIN_FILE, VALIDATION_FILE),
+                                   size=EMBEDDING_SIZE,
+                                   min_count=MINIMAL_WORD_FREQUENCY,
+                                   workers=4)
+    model.save(save_to_path)
+
+
+def evaluate(path):
+
+    print("Loading word2vec model from {}".format(path))
+    model = gensim.models.Word2Vec.load(path)
+    print('Vocabulary size is: {}'.format(len(model.wv.vocab)))
+    print(model.most_similar(
+        positive=['woman', 'king'], negative=['man'], topn=5))
+    print(model.most_similar(
+        positive=['girl', 'man'], negative=['boy'], topn=5))
+    print(model.most_similar(
+        positive=['head', 'eye', 'lips', 'nose'], negative=['toes'], topn=5))
+    print(model.most_similar(
+        positive=['pain'], topn=10))
+    print(model.most_similar(
+        positive=['pain'], negative=['happy'], topn=10))
+
+    x = model[model.wv.vocab]
+    pca = PCA(n_components=2)
+    x_pca = pca.fit_transform(x[:5000,:])
+
+    tsne = TSNE(n_components=2, random_state=0)
+    x_tsne = tsne.fit_transform(x[:5000, :])
+
+    plt.scatter(x_pca[:, 0], x_pca[:, 1])
+    for label, x, y in zip(model.wv.vocab, x_pca[:, 0], x_pca[:, 1]):
+        plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points')
+    plt.show()
+
+    plt.scatter(x_tsne[:, 0], x_tsne[:, 1])
+    for label, x, y in zip(model.wv.vocab, x_tsne[:, 0], x_tsne[:, 1]):
+        plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points')
+    plt.show()
+
+
+def main():
+    # train_embeddings(WORD2VEC)
+    evaluate(WORD2VEC)
+
+if __name__ == "__main__":
+    main()
+
+
+
